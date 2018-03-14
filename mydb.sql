6f89d247a5e146357806c97ce816c75e94e41982
Evania <evan.nhea@gmail.com>
2018-03-14 17:08:24 +0700

    add table user in mydb

--- mydb.sql
+++ mydb.sql
@@ -31,7 +31,7 @@ CREATE TABLE `students` (
   `date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(60) NOT NULL,
   PRIMARY KEY (`studentID`)
-) ENGINE=InnoDB AUTO_INCREMENT=18004 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18008 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,8 +40,33 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (14,'Annabelle Sucipto','Jl Mangkubumi no. 29','F','1999-12-13','2018-01-12 00:00:00','annabelle@gmail.com'),(16,'Diana Sinta W','Jl Gejayan 09 Yogyakarta','F','1999-02-22','2018-01-19 00:00:00','diana@gmail.com'),(17,'Amanda Putri','Jl Arum Manis','F','1999-08-08','2018-02-20 00:00:00','amanda@gmail.com'),(18,'Alvaro Putra','Jl Gejayan 112','M','1999-09-19','2018-03-11 20:28:18','alvaro@gmail.com'),(20,'Anindi Pratiwi','Jl. Magelang no. 25 Yogyakarta','F','1999-09-09','2018-03-12 00:00:00','anindi@gmail.com'),(21,'Dion Rumbaka','Jl. Colombo no. 17 Yogyakarta','M','1999-08-21','2018-03-13 10:16:47','dion@gmail.com'),(22,'Ratna Sari Dewi','Jl. Laksda Adi Sucipto no. 8','F','1999-03-11','2018-03-13 10:17:18','ratna@gmail.com'),(23,'Angga Maulana','Jl Mangga no 12 Yogyakarta','M','1999-01-12','2018-03-13 10:17:45','angga@gmail.com'),(18002,'Melati Kusuma','Jl Cempaka no 11 Yogyakarta','F','1999-04-04','2018-03-13 11:27:23','melati@gmail.com'),(18003,'Nuansa Pagi','Jl Afandi no 45 Yogyakarta','M','2018-08-07','2018-03-13 11:36:41','nuansa@gmail.com');
+INSERT INTO `students` VALUES (14,'Annabelle Sucipto','Jl Mangkubumi no. 29','F','1999-12-13','2018-01-12 00:00:00','annabelle@gmail.com'),(16,'Diana Sinta W','Jl Gejayan 09 Yogyakarta','F','1999-02-22','2018-01-19 00:00:00','diana@gmail.com'),(17,'Amanda Putri','Jl Arum Manis','F','1999-08-08','2018-02-20 00:00:00','amanda@gmail.com'),(18,'Alvaro Putra','Jl Gejayan 112','M','1999-09-19','2018-03-11 20:28:18','alvaro@gmail.com'),(20,'Anindi Pratiwi','Jl. Magelang no. 25 Yogyakarta','F','1999-09-09','2018-03-12 00:00:00','anindi@gmail.com'),(21,'Dion Rumbaka','Jl. Colombo no. 17 Yogyakarta','M','1999-08-21','2018-03-13 10:16:47','dion@gmail.com'),(22,'Ratna Sari Dewi','Jl. Laksda Adi Sucipto no. 8','F','1999-03-11','2018-03-13 10:17:18','ratna@gmail.com'),(23,'Angga Maulana','Jl Mangga no 12 Yogyakarta','M','1999-01-12','2018-03-13 10:17:45','angga@gmail.com'),(18002,'Melati Kusuma','Jl Cempaka no 11 Yogyakarta','F','1999-04-04','2018-03-13 11:27:23','melati@gmail.com'),(18003,'Nuansa Pagi','Jl Afandi no 45 Yogyakarta','M','2018-08-07','2018-03-13 11:36:41','nuansa@gmail.com'),(18004,'John Len','Jl Manunggal no 6','M','1999-11-12','2018-03-13 16:28:46','johnlenon@gmail.com'),(18005,'Murni Sentosa','Jl Perkutut no 23','F','1999-01-01','2018-03-14 15:50:08','murni@gmail.com'),(18006,'Thomas Runner','JL Ayam Jago','M','1999-11-11','2018-03-14 16:54:23','thomas@gmail.com'),(18007,'Maya Septha','Jl Jakarta','F','1998-06-01','2018-03-14 17:04:53','maya.s@gmail.com');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','4de6c29ec17a9b735f00ec365e9c3adb60690635');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -53,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-13 14:55:35
+-- Dump completed on 2018-03-14 17:08:01
